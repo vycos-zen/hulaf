08d53fb155694fa8489bbd4118b33f8b19eb536e
peterkollerlv <peterkollerlv@live.com>
2018-11-23 21:02:49 +0100

    generator implementation

---
+++ HULAF.CodeGen/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using HULAF.CodeGenLib;
+
+namespace HULAF.CodeGen
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var generator = new Generator();
+            Console.WriteLine(string.Concat("Generated: ", Environment.NewLine, await generator.Generate()));
+
+
+        }
+    }
+}
