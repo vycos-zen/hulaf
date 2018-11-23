b6c2dc306a997c3441e01592658cb0a35b2b91f4
peterkollerlv <peterkollerlv@live.com>
2018-11-23 07:27:32 +0100

    code generation poc

---
+++ HULAF.CodeGen/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Reinforced.Typings.Attributes;
+
+namespace HULAF.CodeGen
+{
+    [TsInterface] 
+    public class Class1
+    {
+        public string Id {get; set;}
+    }
+}
